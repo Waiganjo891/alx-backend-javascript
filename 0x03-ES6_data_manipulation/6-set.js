7ca6e61e8a7309faa08cc791ec792ae2802fe453
Your Name <you@example.com>
2024-08-04 07:18:25 -0700

    Task 6

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,10 @@
+/**
+ * Converts an array into a Set.
+ * @param {Array} array - The array to convert.
+ * @returns {Set} - The resulting Set.
+ */
+function setFromArray(array) {
+    return new Set(array);
+}
+
+export default setFromArray;
