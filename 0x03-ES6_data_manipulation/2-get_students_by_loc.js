5b24bf78305e346f568924ffd98536d20758361b
Your Name <you@example.com>
2024-08-04 06:23:29 -0700

    Task 2

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,13 @@
+// File: 2-get_students_by_loc.js
+
+/**
+ * Function to filter students by location
+ * @param {Array} students - List of student objects
+ * @param {string} city - The city to filter students by
+ * @returns {Array} - Array of students located in the specified city
+ */
+function getStudentsByLocation(students, city) {
+    return students.filter(student => student.location === city);
+}
+
+module.exports = getStudentsByLocation;
