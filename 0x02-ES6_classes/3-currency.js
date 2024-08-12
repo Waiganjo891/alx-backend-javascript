522bbac28bbdf8ff2d262a754d42bcb76043c224
Your Name <you@example.com>
2024-08-12 05:08:23 -0700

    Task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,31 @@
+// File: 3-currency.js
+
+class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
+
+const currency = new Currency('USD', 'United States Dollar');
+console.log(currency.displayFullCurrency());
