8ea406060a06dfc221b2599ba9527672c6c24e8c
Your Name <you@example.com>
2024-08-20 00:22:25 -0700

    Task 6

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,16 @@
+// Create a small HTTP server using Express
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+const PORT = 1245;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
