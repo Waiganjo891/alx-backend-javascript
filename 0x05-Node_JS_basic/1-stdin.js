cd1a7033d7d0ce9fdd34387d992e0c5b86277e62
Your Name <you@example.com>
2024-08-19 22:37:46 -0700

    Task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,23 +1,19 @@
-#!/usr/bin/env node
+// Create an interface for reading input
 
 const readline = require('readline');
 
-// Create an interface for reading input
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-// Display welcome message
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-// Capture user input
 rl.on('line', (input) => {
     console.log(`Your name is: ${input}`);
     rl.close();
 });
 
-// Handle the readline close event
 rl.on('close', () => {
     console.log("This important software is now closing");
 });
