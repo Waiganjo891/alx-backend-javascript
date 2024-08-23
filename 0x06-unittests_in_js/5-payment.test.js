269045e9e2bc311d7e2f14f7f292f3cff1cc1c82
Your Name <you@example.com>
2024-08-23 03:03:17 -0700

    Task 5

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,28 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./5-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function() {
+  let consoleSpy;
+
+  beforeEach(function() {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(function() {
+    consoleSpy.restore();
+  });
+
+  it('should log the correct total when called with 100 and 20', function() {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+  });
+
+  it('should log the correct total when called with 10 and 10', function() {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 20')).to.be.true;
+  });
+});
