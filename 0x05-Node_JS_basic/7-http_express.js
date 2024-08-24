5e445e81e4b33828e3864d09f5ce4619e84ea5ab
Your Name <you@example.com>
2024-08-24 01:53:24 -0700

    Task 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -5,26 +5,26 @@ const countStudents = require('./3-read_file_async');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.set('Content-Type', 'text/plain');
-    res.send('Hello Holberton School!');
+  res.set('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-    const databaseFile = process.argv[2];
-    countStudents(databaseFile)
-        .then(() => {
-            res.set('Content-Type', 'text/plain');
-            res.send('This is the list of our students\n');
-        })
-        .catch((error) => {
-            res.set('Content-Type', 'text/plain');
-            res.status(500).send(error.message);
-        });
+  const databaseFile = process.argv[2];
+  countStudents(databaseFile)
+    .then(() => {
+      res.set('Content-Type', 'text/plain');
+      res.send('This is the list of our students\n');
+    })
+    .catch((error) => {
+      res.set('Content-Type', 'text/plain');
+      res.status(500).send(error.message);
+    });
 });
 
 const PORT = 1245;
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
 
 module.exports = app;
