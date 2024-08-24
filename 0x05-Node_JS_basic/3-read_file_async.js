070389bea4b3f7cf82c433ba5c41ffcaedead6ce
Your Name <you@example.com>
2024-08-24 03:52:52 -0700

    Task 3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,13 +1,10 @@
-// File: 3-read_file_async.js
-
 const fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.promises.readFile(path, 'utf-8')
       .then((data) => {
-        const rows = data.split('\n').filter(line => line.trim() !== '');
-        const headers = rows.shift().split(',');
+        const rows = data.split('\n').filter((line) => line.trim() !== '');
         const students = {};
         let totalStudents = 0;
         rows.forEach((row) => {
@@ -24,9 +21,11 @@ function countStudents(path) {
         });
         console.log(`Number of students: ${totalStudents}`);
         for (const field in students) {
-          const count = students[field].length;
-          const list = students[field].join(', ');
-          console.log(`Number of students in ${field}: ${count}. List: ${list}`);
+          if (students.hasOwnProperty(field)) {
+            const count = students[field].length;
+            const list = students[field].join(', ');
+            console.log(`Number of students in ${field}: ${count}. List: ${list}`);
+          }
         }
         resolve();
       })
