eef2184325ac3d08ae83e3482dfd6b9f11f07035
Your Name <you@example.com>
2024-09-02 23:41:39 -0700

    Task 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,5 @@
 // Import the ClassRoom class from 0-classroom.js
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 function initializeRooms() {
   const rooms = [
