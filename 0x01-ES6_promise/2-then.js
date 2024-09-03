3390fb0ca8152cc5c4a500594a310621e075cd84
Your Name <you@example.com>
2024-09-03 00:26:03 -0700

    Task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,7 +3,7 @@
 function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
-      console.log("Got a response from the API");
+      console.log('Got a response from the API');
       return {
         status: 200,
         body: 'success',
