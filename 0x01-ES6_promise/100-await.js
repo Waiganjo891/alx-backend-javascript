2f8e5695df1d5e3b8f7473524496445d0913ce67
Your Name <you@example.com>
2024-09-05 01:55:51 -0700

    Task 100

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,6 +1,6 @@
 // File: 100-await.js
 
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   try {
